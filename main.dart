7623279525fd6b0a37de10556822eac66aa12ee6
jawaher90 <93349046+jawaher90@users.noreply.github.com>
2021-12-05 11:55:37 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,109 +1,34 @@
-import 'dart:ui';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:drag_select_gridview_example/page/select_grid_page.dart';
 import 'package:flutter/material.dart';
-import 'package:gp3funfood/SignupPage.dart';
-import 'package:gp3funfood/login.dart';
+import 'package:flutter/services.dart';
+import 'package:drag_select_gridview_example/Childpage.dart';
 
-void main() async{
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  static final String title = 'Add serving   ';
+
+
+
   @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     body: SafeArea(
-       child: Container(
-         width: double.infinity,
-         height: MediaQuery.of(context).size.height,
-         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: <Widget>[
-             Column(
-               children: const <Widget>[  //i add the const
-                 Text(
-                   "Welcome",
-                    style: TextStyle(
-                     fontWeight: FontWeight.bold,
-                     fontSize: 50,
-                      color: Colors.black45
-                   ),
-                 ),
-                 SizedBox(
-                   height: 40,
-                 ),
-               ],
-             ),
-            Container(
-              height: MediaQuery.of(context).size.height / 2,
-              decoration:  BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/funfood.png")
-                )
-              ),
-            ),
-             Column(
-               children: <Widget>[
-                 // the login button
-                 MaterialButton(
-                   minWidth: double.infinity,
-                   height: 60,
-                   onPressed: (){
-                     Navigator.push(context, MaterialPageRoute(builder: (context) => login()));
-
-                   },
-                 color: Colors.blueAccent,
-                 // defining the shape
-                   shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(50)
-                   ),
-                   child: Text(
-                     "Log in",
-                     style: TextStyle(
-                       fontWeight: FontWeight.w600,
-                       fontSize: 18
-                         ,color: Colors.white
-                     ),
-                   ),
-                 ),
-                 // creating the singup button
-                 SizedBox(height:20),
-                 MaterialButton(
-                   minWidth: double.infinity,
-                   height:60,
-                   onPressed: (){
-                     Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
-
-                   },
-                   color: Colors.redAccent,
-                   shape: RoundedRectangleBorder(
-                       borderRadius: BorderRadius.circular(50)
-                   ),
-                   child: Text("Sign up",style: TextStyle(
-
-                     fontWeight: FontWeight.w600,fontSize: 18,
-
-                   ),),
-                 ),
-
-               ],
-             )
+  Widget build(BuildContext context) =>
 
 
-           ],
-         ),
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: Childpage(),
+      )
+;
 
-     ),
-     ),
-   );
-  }
 }
-
