a258830407cf90a6f08cf59127a08fb88e5c2835
jawaher90 <93349046+jawaher90@users.noreply.github.com>
2022-05-22 21:58:29 +0300

    Delete FunFoodGP2 directory

--- FunFoodGP2/lib/consts.dart
+++
@@ -1,5 +0,0 @@
-
-// for debugging on emulator
-const String url = "http://10.0.2.2:80/food";
-const String serverError = "Server error, don't panic; help on the way :)";
-const String connectionError = "Connectivity error, Please make sure you are connected to the internet:)";
